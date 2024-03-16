db6eb795241c6f228a04dca7ce156a853966a87a
jamkles <114435743+Realjamkles@users.noreply.github.com>
2024-03-16 18:46:32 +0000

    Update jamkles.config.lua

--- jamkles.config.lua
+++ jamkles.config.lua
@@ -1,4 +1,6 @@
--- Instances:
+
+
+-- instances:
 
 local jamkleslua = Instance.new("ScreenGui")
 local jamkles = Instance.new("Frame")
@@ -11,13 +13,14 @@ local ImageButton = Instance.new("ImageButton")
 
 --Properties:
 
+
 jamkleslua.Name = "jamkles.lua"
 jamkleslua.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 jamkleslua.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 jamkles.Name = "jamkles"
 jamkles.Parent = jamkleslua
-jamkles.BackgroundColor3 = Color3.fromRGB(214, 214, 214)
+jamkles.BackgroundColor3 = Color3.fromRGB(27, 42, 53)
 jamkles.BorderColor3 = Color3.fromRGB(0, 0, 0)
 jamkles.BorderSizePixel = 0
 jamkles.Position = UDim2.new(0.233454928, 0, 0.23540315, 0)
@@ -26,14 +29,14 @@ jamkles.Visible = false
 
 jamklescamlock.Name = "jamkles camlock"
 jamklescamlock.Parent = jamkles
-jamklescamlock.BackgroundColor3 = Color3.fromRGB(214, 214, 214)
+jamklescamlock.BackgroundColor3 = Color3.fromRGB(27, 42, 53)
 jamklescamlock.BorderColor3 = Color3.fromRGB(0, 0, 0)
 jamklescamlock.BorderSizePixel = 0
 jamklescamlock.Position = UDim2.new(0.240750387, 0, 0.278962016, 0)
 jamklescamlock.Size = UDim2.new(0, 101, 0, 24)
 jamklescamlock.Font = Enum.Font.SourceSans
 jamklescamlock.Text = "Camlock"
-jamklescamlock.TextColor3 = Color3.fromRGB(0, 0, 0)
+jamklescamlock.TextColor3 = Color3.fromRGB(242, 243, 243)
 jamklescamlock.TextSize = 14.000
 jamklescamlock.MouseButton1Click:Connect(function()
 	loadstring(game:HttpGet("https://pastebin.com/raw/N2WHaEmX", true))()
@@ -45,29 +48,29 @@ UICorner.Parent = jamklescamlock
 
 
 TextLabel.Parent = jamkles
-TextLabel.BackgroundColor3 = Color3.fromRGB(214, 214, 214)
+TextLabel.BackgroundColor3 = Color3.fromRGB(27, 42, 53)
 TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.BorderSizePixel = 0
 TextLabel.Position = UDim2.new(-0.00310426112, 0, -0.00303101446, 0)
 TextLabel.Size = UDim2.new(0, 186, 0, 50)
 TextLabel.Font = Enum.Font.SourceSans
 TextLabel.Text = "welcome to jamkles lua"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextColor3 = Color3.fromRGB(242, 243, 243)
 TextLabel.TextSize = 14.000
 
 TextLabel_2.Parent = jamkles
-TextLabel_2.BackgroundColor3 = Color3.fromRGB(214, 214, 214)
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(27, 42, 53)
 TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel_2.BorderSizePixel = 0
 TextLabel_2.Position = UDim2.new(0.00764842704, 0, 0.818670213, 0)
 TextLabel_2.Size = UDim2.new(0, 184, 0, 50)
 TextLabel_2.Font = Enum.Font.SourceSans
 TextLabel_2.Text = "discord.gg/9qKxx6DCRM"
-TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextColor3 = Color3.fromRGB((242, 243, 243)
 TextLabel_2.TextSize = 14.000
 
 ImageButton.Parent = jamkleslua
-ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundColor3 = Color3.fromRGB(27, 42, 53)
 ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.184992179, 0, 0.105653375, 0)
@@ -94,4 +97,3 @@ local function BURSVSV_fake_script() -- ImageButton.OPEN/CLOSE SCRIPT
 	
 end
 coroutine.wrap(BURSVSV_fake_script)()
-
