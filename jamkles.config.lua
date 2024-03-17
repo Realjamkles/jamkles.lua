6e3a2585ca8b6d983fcf4682d6f016e969a273ff
jamkles <114435743+Realjamkles@users.noreply.github.com>
2024-03-17 00:09:21 +0000

    Update jamkles.config.lua

--- jamkles.config.lua
+++ jamkles.config.lua
@@ -1,149 +1,169 @@
-
 -- Gui to Lua
 -- Version: 3.2
 
 -- Instances:
 
-local CorrectKey = "ZZZZ", ”abc” -- YOUR KEY HERE
-
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
+local jamkles_lua = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local jamkles = Instance.new("Frame")
+local jamklescamlock = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
+local silentaim = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
-local TextBox = Instance.new("TextBox")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local resolver = Instance.new("TextButton")
 local UICorner_3 = Instance.new("UICorner")
-local TextButton = Instance.new("TextButton")
-local UICorner_4 = Instance.new("UICorner")
-local TextButton_2 = Instance.new("TextButton")
-local UICorner_5 = Instance.new("UICorner")
-local TextButton_3 = Instance.new("TextButton")
-local UICorner_6 = Instance.new("UICorner")
-local TextButton_4 = Instance.new("TextButton")
-local UICorner_7 = Instance.new("UICorner")
-local TextButton_5 = Instance.new("TextButton")
 
 --Properties:
 
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+jamkles_lua.Name = "jamkles_lua"
+jamkles_lua.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+jamkles_lua.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.5, -199, 0.9, -280)
-Frame.Size = UDim2.new(0, 352, 0, 283)
-Frame.Active = true
-Frame.Draggable = true
+ImageButton.Parent = jamkles_lua
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.140561566, 0, 0.0817588419, 0)
+ImageButton.Size = UDim2.new(0, 43, 0, 44)
+ImageButton.Image = "rbxassetid://16723980638"
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(38, 0, 255)
+jamkles.Name = "jamkles"
+jamkles.Parent = jamkles_lua
+jamkles.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+jamkles.BorderColor3 = Color3.fromRGB(0, 0, 0)
+jamkles.BorderSizePixel = 0
+jamkles.Position = UDim2.new(0.233454928, 0, 0.23540315, 0)
+jamkles.Size = UDim2.new(0, 186, 0, 281)
+jamkles.Visible = false
+
+jamklescamlock.Name = "jamkles camlock"
+jamklescamlock.Parent = jamkles
+jamklescamlock.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+jamklescamlock.BorderColor3 = Color3.fromRGB(0, 0, 0)
+jamklescamlock.BorderSizePixel = 0
+jamklescamlock.Position = UDim2.new(0.240750387, 0, 0.278962016, 0)
+jamklescamlock.Size = UDim2.new(0, 101, 0, 24)
+jamklescamlock.Font = Enum.Font.SourceSans
+jamklescamlock.Text = "Camlock"
+jamklescamlock.TextColor3 = Color3.fromRGB(255, 255, 255)
+jamklescamlock.TextSize = 14.000
+jamklescamlock.MouseButton1Click:Connect(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/N2WHaEmX", true))()
+  end)
+
+UICorner.CornerRadius = UDim.new(0.150000006, 0)
+UICorner.Parent = jamklescamlock
+
+silentaim.Name = "silent aim"
+silentaim.Parent = jamkles
+silentaim.BackgroundColor3 = Color3.fromRGB(3, 3, 3)
+silentaim.BorderColor3 = Color3.fromRGB(0, 0, 0)
+silentaim.BorderSizePixel = 0
+silentaim.Position = UDim2.new(0.240750223, 0, 0.3643713, 0)
+silentaim.Size = UDim2.new(0, 101, 0, 24)
+silentaim.Font = Enum.Font.SourceSans
+silentaim.Text = "silent aim"
+silentaim.TextColor3 = Color3.fromRGB(255, 255, 255)
+silentaim.TextSize = 14.000
+silentaim.MouseButton1Click:Connect(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/B1ufXMph", true))()
+
+  end)
+
+
+UICorner_2.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_2.Parent = silentaim
+
+TextLabel.Parent = jamkles
+TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.0767045468, 0, 0.123674914, 0)
-TextLabel.Size = UDim2.new(0, 298, 0, 46)
-TextLabel.Font = Enum.Font.GothamBlack
-TextLabel.Text = "jamkles lua " 
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.Position = UDim2.new(0.00764842704, 0, -0.00303101446, 0)
+TextLabel.Size = UDim2.new(0, 184, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "welcome to jamkles lua"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 14.000
 
-UICorner.Parent = TextLabel
+TextLabel_2.Parent = jamkles
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.00764842704, 0, 0.818670213, 0)
+TextLabel_2.Size = UDim2.new(0, 184, 0, 50)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "discord.gg/9qKxx6DCRM"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 14.000
 
-UICorner_2.Parent = Frame
+resolver.Name = "resolver"
+resolver.Parent = jamkles
+resolver.BackgroundColor3 = Color3.fromRGB(3, 3, 3)
+resolver.BorderColor3 = Color3.fromRGB(0, 0, 0)
+resolver.BorderSizePixel = 0
+resolver.Position = UDim2.new(0.240750223, 0, 0.456898004, 0)
+resolver.Size = UDim2.new(0, 101, 0, 24)
+resolver.Font = Enum.Font.SourceSans
+resolver.Text = "resolver "
+resolver.TextColor3 = Color3.fromRGB(255, 255, 255)
+resolver.TextSize = 14.000
+resolver.MouseButton1Click:Connect(function()
+    local RunService = game:GetService("RunService")
 
-TextBox.Parent = Frame
-TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextBox.BorderSizePixel = 0
-TextBox.Position = UDim2.new(0.127840906, 0, 0.349823326, 0)
-TextBox.Size = UDim2.new(0, 261, 0, 28)
-TextBox.Font = Enum.Font.SourceSans
-TextBox.Text = "ENTER KEY"
-TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextBox.TextSize = 14.000
-
-UICorner_3.Parent = TextBox
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(85, 0, 255)
-TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.127840906, 0, 0.477031797, 0)
-TextButton.Size = UDim2.new(0, 120, 0, 30)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "CHECK KEY" 
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextSize = 14.000
-TextButton.MouseButton1Click:Connect(function()
-if TextBox.Text == CorrectKey then
-ScreenGui:Destroy()
+    local function zeroOutYVelocity(hrp)
+        hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)
+        hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)
+    end
     
-    loadstring(game:HttpGet("https://pastebin.com/raw/ggsRCZD4", true))() 
+    local function onPlayerAdded(player)
+        player.CharacterAdded:Connect(function(character)
+            local hrp = character:WaitForChild("HumanoidRootPart")
+            zeroOutYVelocity(hrp)
+        end)
+    end
+    
+    local function onPlayerRemoving(player)
+        player.CharacterAdded:Disconnect()
+    end
+    
+    game.Players.PlayerAdded:Connect(onPlayerAdded)
+    game.Players.PlayerRemoving:Connect(onPlayerRemoving)
+    
+    RunService.Heartbeat:Connect(function()
+        pcall(function()
+            for i, player in pairs(game.Players:GetChildren()) do
+                if player.Name ~= game.Players.LocalPlayer.Name then
+                    local hrp = player.Character.HumanoidRootPart
+                    zeroOutYVelocity(hrp)
+                end
+            end
+        end)
+    end)
 
-end   
-end)
+  end)
 
-UICorner_4.Parent = TextButton
+UICorner_3.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_3.Parent = resolver
 
-TextButton_2.Parent = Frame
-TextButton_2.BackgroundColor3 = Color3.fromRGB(30, 0, 255)
-TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.BorderSizePixel = 0
-TextButton_2.Position = UDim2.new(0.528409064, 0, 0.477031797, 0)
-TextButton_2.Size = UDim2.new(0, 120, 0, 30)
-TextButton_2.Font = Enum.Font.SourceSans
-TextButton_2.Text = "CLEAR"
-TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.TextSize = 14.000
-TextButton_2.MouseButton1Click:Connect(function()
-    TextBox.Text = ""
-end)
+-- Scripts:
 
-UICorner_5.Parent = TextButton_2
+local function EEFKFNY_fake_script() -- jamkles_lua.LocalScript 
+	local script = Instance.new('LocalScript', jamkles_lua)
 
-TextButton_3.Parent = Frame
-TextButton_3.BackgroundColor3 = Color3.fromRGB(43, 0, 255)
-TextButton_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_3.BorderSizePixel = 0
-TextButton_3.Position = UDim2.new(0.326704532, 0, 0.614840984, 0)
-TextButton_3.Size = UDim2.new(0, 120, 0, 35)
-TextButton_3.Font = Enum.Font.SourceSans
-TextButton_3.Text = "GET KEY"
-TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_3.TextSize = 14.000
-TextButton_3.MouseButton1Click:Connect(function()
-    setclipboard("https://discord.gg/9qKxx6DCRM") -- PUT THE LINK WITH THE KEY HERE
-end)
+	frame = script.Parent.jamkles
+	frame.Draggable = true
+	frame.Selectable = true
+	frame.Active = true
+end
+coroutine.wrap(EEFKFNY_fake_script)()
+local function VPWJLQ_fake_script() -- ImageButton.OPEN/CLOSE SCRIPT 
+	local script = Instance.new('LocalScript', ImageButton)
 
-UICorner_6.Parent = TextButton_3
-
-TextButton_4.Parent = Frame
-TextButton_4.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-TextButton_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_4.BorderSizePixel = 0
-TextButton_4.Position = UDim2.new(0.272727281, 0, 0.777385175, 0)
-TextButton_4.Size = UDim2.new(0, 160, 0, 42)
-TextButton_4.Font = Enum.Font.SourceSans
-TextButton_4.Text = "JOIN MY DISCORD"
-TextButton_4.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_4.TextSize = 14.000
-TextButton_4.MouseButton1Click:Connect(function()
-    setclipboard("Y https://discord.gg/9qKxx6DCRM")
-end)
-
-UICorner_7.Parent = TextButton_4
-
-TextButton_5.Parent = Frame
-TextButton_5.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_5.BorderSizePixel = 0
-TextButton_5.Position = UDim2.new(0.900568187, 0, 0.0282685515, 0)
-TextButton_5.Size = UDim2.new(0, 29, 0, 21)
-TextButton_5.Font = Enum.Font.GothamBlack
-TextButton_5.Text = "X"
-TextButton_5.TextColor3 = Color3.fromRGB(255, 0, 4)
-TextButton_5.TextSize = 14.000
-TextButton_5.MouseButton1Click:Connect(function()
-wait(0.5)
-    ScreenGui:Destroy()
-end)
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.jamkles.Visible = not script.Parent.Parent.jamkles.Visible
+	end)
+	
+end
+coroutine.wrap(VPWJLQ_fake_script)()
